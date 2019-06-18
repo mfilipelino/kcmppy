9d12af58ec5bd93b736fce4423760914dca66ce7
Marcos Filipe Gomes Da Silva Lino <marcos.lino@b2wdigital.com>
2019-06-18 11:39:04 -0300

    consumer

---
+++ consumer.py
@@ -0,0 +1,42 @@
+from kafka import KafkaConsumer
+import sys
+from multiprocessing import Pool
+
+TOPIC = 'bwpkpr'
+GROUP_ID = 'D'
+BOOTSTRAP_SERVERS = ['back1145.back.b2w:9092',
+                     'back1146.back.b2w:9092',
+                     'back1151.back.b2w:9092',
+                     'back1152.back.b2w:9092',
+                     'back1153.back.b2w:9092',
+                     'back1154.back.b2w:9092',
+                     'back1155.back.b2w:9092'
+                     ]
+
+AUTO_OFFSET_RESET = 'earliest'
+ENABLE_AUTO_COMMIT = True
+CONSUMER_TIMETOUT_MS = 100000
+
+
+def run(process_id):
+    consumer = KafkaConsumer(TOPIC,
+                             group_id=GROUP_ID,
+                             auto_offset_reset=AUTO_OFFSET_RESET,
+                             enable_auto_commit=ENABLE_AUTO_COMMIT,
+                             consumer_timeout_ms=CONSUMER_TIMETOUT_MS,
+                             bootstrap_servers=BOOTSTRAP_SERVERS)
+
+    with open('process_' + str(process_id) + '.out', 'w') as outfile:
+        for msg in consumer:
+            s = 'process_id: {process_id} topic: {topic}, partition: {partition} offset: {offset}\n'.format(
+                process_id=process_id,
+                topic=msg.topic,
+                partition=msg.partition,
+                offset=msg.offset)
+            outfile.write(s)
+
+
+if __name__ == '__main__':
+    number_process = int(sys.argv[1])
+    with Pool(number_process) as pool:
+        pool.map(run, range(number_process))
